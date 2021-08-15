df4c0fc848f107a0f4c3b377eb4f829457de344f
DuxPal <88507976+DuxPal@users.noreply.github.com>
2021-08-15 23:48:51 +0530

    Update main.py

--- Simple-Assembler/main.py
+++ Simple-Assembler/main.py
@@ -2,7 +2,8 @@ addr = 0
 string_list = []
 var_dict = {}
 label_dict = {}
-
+line_number=1
+halt_occured=False
 
 def process():
     for i in string_list:
@@ -41,7 +42,14 @@ inst_dict = {"add": "00000",
 
 
 def checking(arr):
+    global halt_occured
+    if halt_occured:
+        print("Halt has occured can't take anymore statements")
+        exit()
+
     result = ""
+   
+    global line_number
 
 
     if arr[0][-1] == ":":
@@ -53,7 +61,9 @@ def checking(arr):
     if length == 2:
         if arr[0] in inst_dict:  # Type E
             result += inst_dict[arr[0]] + "0"*3 + f'{label_dict[arr[1]]:08b}'
-
+        else:
+            print("This is not a type E statement but is 2 letters long, error at line number: "+str(line_number+len(var_dict)))
+            exit()
     elif length == 3:
         if arr[0] == "mov" and arr[2][0] == "$" and arr[1] in reg_dict:  # Type B
             result += "00010" + reg_dict[arr[1]] + f'{int(arr[2][1:]):08b}'
@@ -69,18 +79,25 @@ def checking(arr):
 
         elif arr[0] in inst_dict and arr[1] in reg_dict and arr[2] in var_dict:
             result += inst_dict[arr[0]] + reg_dict[arr[1]] + f'{var_dict[arr[2]]:08b}'
+        else:
+            print("This is not a type B or C statement but is 3 letters long, error at line number: "+str(line_number+len(var_dict)))
+            exit()
 
     elif length == 4:
         if arr[0] in inst_dict and arr[1] in reg_dict and arr[2] in reg_dict:  # Type A
             result += inst_dict[arr[0]] + "00" + reg_dict[arr[1]] + reg_dict[arr[2]] + reg_dict[arr[3]]
+        else:
+            print("This is not a type A statement but is 3 letters long, error at line number: "+str(line_number+len(var_dict)))
+            exit()
 
     elif length == 1:
         if arr[0] in inst_dict:
             result += inst_dict[arr[0]] + "0" * 11
+            halt_occured=True
     else:
-        print("error")
+        print("syntax error at line number: "+str(line_number+len(var_dict)))
         exit()
-
+    line_number+=1
     print(result)
 
 
@@ -92,11 +109,17 @@ def main():
                 ln = line.strip()
                 if not ln[0:3] == "var":
                     string_list.append(ln)
+                global line_number
 
-                #string_list.append(ln)
+                
                 Ln = ln.split()
                 if Ln[0] == "var":
-                    var_dict[Ln[1]] = 0
+                    try:
+                        var_dict[Ln[1]] = 0
+                    except:
+                        print("Pass the variable name at line number "+ str(line_number))
+                        exit()
+                    
 
                     # label:
                 else:
